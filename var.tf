fa1d0cb923249365d0fe22fbd65e78f659a39fe7
veenayaraddi <veenashankar.yaraddi@hcltech.com>
2025-03-16 10:22:59 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {}
+variable "vpc_cidr" {}
+variable "subnet_cidr_1" {}
+variable "subnet_cidr_2" {}
+variable "subnet_cidr_3" {}
+variable "az_1" {}
+variable "az_2" {}
+variable "az_3" {}
+variable "ami_id" {}
+variable "instance_type" {}
